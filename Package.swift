e89e4e0904f24227c1ef8202f6601e0990cea819
Philip Turner <philipturner.AR@gmail.com>
2021-12-07 12:13:40 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "ARHeadsetUtil", url: "https://github.com/philipturner/ARHeadsetUtil", branch: "temp"),
+        .package(name: "ARHeadsetUtil", url: "https://github.com/philipturner/ARHeadsetUtil", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
