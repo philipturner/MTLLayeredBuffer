14447effb03021bd043fbce62c916ccfc25f07e8
Philip Turner <philipturner.AR@gmail.com>
2021-12-07 12:10:10 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MTLayeredBufferModule",
+    name: "MTLLayeredBufferModule",
     platforms: [
         .iOS(.v11),
         .macOS(.v10_11),
@@ -13,8 +13,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MTLayeredBufferModule",
-            targets: ["MTLayeredBufferModule"]),
+            name: "MTLLayeredBufferModule",
+            targets: ["MTLLayeredBufferModule"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,10 +24,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MTLayeredBufferModule",
+            name: "MTLLayeredBufferModule",
             dependencies: ["ARHeadsetUtil"]),
         .testTarget(
-            name: "MTLayeredBufferModuleTests",
-            dependencies: ["MTLayeredBufferModuleTests"]),
+            name: "MTLLayeredBufferModuleTests",
+            dependencies: ["MTLLayeredBufferModuleTests"]),
     ]
 )
